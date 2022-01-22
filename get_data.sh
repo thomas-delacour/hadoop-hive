ad89d119b44e502bd68ed49375dbe348fb95a9d5
Thomas Delacour <thomas.delacour@protonmail.com>
2022-01-22 12:36:45 +0100

    Bash script that will retrieve data from IMDB

---
+++ get_data.sh
@@ -0,0 +1,9 @@
+wget https://datasets.imdbws.com/name.basics.tsv.gz -P data/
+wget https://datasets.imdbws.com/title.akas.tsv.gz -P data/
+wget https://datasets.imdbws.com/title.basics.tsv.gz -P data/
+wget https://datasets.imdbws.com/title.crew.tsv.gz -P data/
+wget https://datasets.imdbws.com/title.episode.tsv.gz -P data/
+wget https://datasets.imdbws.com/title.principals.tsv.gz -P data/
+wget https://datasets.imdbws.com/title.ratings.tsv.gz -P data/
+
+gunzip data/*.gz
