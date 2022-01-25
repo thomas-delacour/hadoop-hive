7787a799a3bfc554f20eaac78dc75ab40a0b4601
Thomas Delacour <thomas.delacour@protonmail.com>
2022-01-25 18:56:44 +0100

    Script to add files to hdfs

---
+++ data_hdfs.sh
@@ -0,0 +1,7 @@
+hdfs dfs -put /data/title.akas.tsv /title.akas.tsv
+hdfs dfs -put /data/title.basics.tsv /title.basics.tsv
+hdfs dfs -put /data/title.crew.tsv /title.crew.tsv
+hdfs dfs -put /data/title.episode.tsv /title.episode.tsv
+hdfs dfs -put /data/title.principals.tsv /title.principals.tsv
+hdfs dfs -put /data/title.ratings.tsv /title.ratings.tsv
+hdfs dfs -put /data/name.basics.tsv /name.basics.tsv
